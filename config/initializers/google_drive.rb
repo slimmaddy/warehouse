d1200fe363b2138d2a3d47756f42de9c5d644944
minhdo <minh.do@wego.com>
2023-04-15 15:45:14 +0700

    change credential path

--- config/initializers/google_drive.rb
+++ config/initializers/google_drive.rb
@@ -5,6 +5,6 @@ Google::Apis.logger.level = Logger::ERROR
 
 Rails.application.config.google_drive = GoogleDrive::DriveService.new
 Rails.application.config.google_drive.authorization = Google::Auth::ServiceAccountCredentials.make_creds(
-  json_key_io: File.open(Rails.root.join('config/google_credentials.json')),
+  json_key_io: File.open(Rails.root.join('google_credentials.json')),
   scope: GoogleDrive::AUTH_DRIVE
 )
