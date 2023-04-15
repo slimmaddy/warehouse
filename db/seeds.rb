b892e4370ac8f431408a55368bc89478f628f5d9
minhdo <minh.do@wego.com>
2023-04-15 16:11:50 +0700

    add seed admin for production

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,3 +6,15 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 AdminUser.create!(email: 'admin@example.com', password: 'password', password_confirmation: 'password') if Rails.env.development?
+if Rails.env.production?
+  email = ENV['ADMIN_EMAIL']
+  password = ENV['ADMIN_PASSWORD']
+  if email.present? && password.present?
+    admin = AdminUser.find_or_initialize_by(email: email)
+    admin.password = password
+    admin.password_confirmation = password
+    admin.save!
+  else
+    raise 'ADMIN_EMAIL and ADMIN_PASSWORD must be set in production environment'
+  end
+end
